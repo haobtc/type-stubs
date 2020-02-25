aa367d3fce1fdf2ee536025ccb8f4ac6ee412fce
Zeng Ke <superisaac.ke@gmail.com>
2020-02-25 16:20:20 +0800

    bitmex

---
+++ bitmex/__init__.pyi
@@ -0,0 +1,14 @@
+from typing import Dict, List, Any
+class BitmexExecution:
+    def Execution_get(self, symbol: str='',
+                    reverse: bool=False,
+                    count: int=20) -> List[Dict[str, Any]]: ...
+    
+class bitmex:
+    Execution: BitmexExecution
+    def __init__(self, *args, **kwargs):
+        ...
+                    
+
+    
+        
