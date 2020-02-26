cd8f28683c19f4a7a1511b22c46591d64d63180b
Zeng Ke <superisaac.ke@gmail.com>
2020-02-26 10:40:02 +0800

    removed trailing spaces

--- bitmex/__init__.pyi
+++ bitmex/__init__.pyi
@@ -3,12 +3,8 @@ class BitmexExecution:
     def Execution_get(self, symbol: str='',
                     reverse: bool=False,
                     count: int=20) -> List[Dict[str, Any]]: ...
-    
+
 class bitmex:
     Execution: BitmexExecution
     def __init__(self, *args, **kwargs):
         ...
-                    
-
-    
-        
