72ff16a1d466e5d4c5ee8c42fb23a5d95defbeae
Zeng Ke <superisaac.ke@gmail.com>
2020-05-22 18:52:27 +0800

    added djang.db.models.aggregates

---
+++ django/db/models/aggregates.pyi
@@ -0,0 +1,10 @@
+from typing import Any
+
+class Sum:
+    def __init__(self, *fields:str, **kwargs:Any): ...
+
+class Count:
+    def __init__(self, *fields:str, **kwargs:Any): ...
+
+class Max:
+    def __init__(self, *fields:str, **kwargs:Any): ...
